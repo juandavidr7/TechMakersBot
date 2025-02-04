95f3d0b73ea145a0f17ba40281529afca78d0717
Juan David Rincón López <66708270+juandavidr7@users.noreply.github.com>
2025-02-04 00:58:28 -0500

    Update chatbot.ts

--- src/app/pages/api/chatbot.ts
+++ src/app/pages/api/chatbot.ts
@@ -1,6 +1,6 @@
 export const sendChatMessage = async (query: string) => {
     try {
-        const response = await fetch("http://127.0.0.1:8060/chatbot/", {
+        const response = await fetch("http://127.0.0.1:8092/chatbot/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ query }),
